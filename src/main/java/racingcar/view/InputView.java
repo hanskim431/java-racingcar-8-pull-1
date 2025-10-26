4950d8b1a4231d4176369e1e133a601e5a6740fd
fogy431 <fogy431@gmail.com>
2025-10-27 08:26:02 +0900

    style(view): 불필요한 개행 삭제

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -4,7 +4,6 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class InputView {
 
-
     public String inputCarNames() {
         final String REQUEST_CAR_NAMES_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
         System.out.println(REQUEST_CAR_NAMES_MESSAGE);
