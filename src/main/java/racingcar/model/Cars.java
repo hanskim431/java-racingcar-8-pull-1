9f309cdef1b9df75e916132b15d22218b9af06e0
fogy431 <fogy431@gmail.com>
2025-10-27 13:10:27 +0900

    feat(model): Cars 일급 컬렉션 구현
    - Iterable<Car> 인터페이스 구현
    - List.copyOf로 방어적 복사 및 불변성 보장

---
+++ src/main/java/racingcar/model/Cars.java
@@ -0,0 +1,17 @@
+package racingcar.model;
+
+import java.util.Iterator;
+import java.util.List;
+
+public class Cars implements Iterable<Car>{
+    private final List<Car> cars;
+
+    public Cars(List<Car> cars) {
+        this.cars = List.copyOf(cars);
+    }
+
+    @Override
+    public Iterator<Car> iterator() {
+        return cars.iterator();
+    }
+}
